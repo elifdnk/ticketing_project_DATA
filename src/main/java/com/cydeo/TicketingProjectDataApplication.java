a17ee78b865715fa8fab0c973de749e15d0b2d5d
ELİF DENK <elifdenkk@gmail.com>
2023-09-28 23:14:32 +0300

    mapper method and @bean annotation added

--- src/main/java/com/cydeo/TicketingProjectDataApplication.java
+++ src/main/java/com/cydeo/TicketingProjectDataApplication.java
@@ -1,13 +1,22 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication  // This includes @Configurations
 public class TicketingProjectDataApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TicketingProjectDataApplication.class, args);
+    }
+
+    // Write a method which return the obj that you trying to add in the container
+    //                 annotate this method with @Bean
+   @Bean
+    public ModelMapper mapper(){
+        return new ModelMapper();
     }
 
 }
