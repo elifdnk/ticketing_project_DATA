d7083ba7909775cd905228da608b7d726a1530a2
ELİF DENK <elifdenkk@gmail.com>
2023-09-28 23:13:04 +0300

    some changes

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -7,9 +7,12 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 @Service
 public class UserServiceImpl implements UserService {
+
+
     @Override
     public List<UserDTO> listAllUsers() {
-        return null;
+
+  return null;
     }
 
     @Override
