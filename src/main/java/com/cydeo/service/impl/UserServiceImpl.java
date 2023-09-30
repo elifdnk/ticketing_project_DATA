e4670e0a43a74500ab5fca4a5ea593546acc667f
ELİF DENK <elifdenkk@gmail.com>
2023-09-30 03:04:05 +0300

    delete button is done

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -41,7 +41,8 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public void deleteByUserName(String user) {
+    public void deleteByUserName(String username) {
+     userRepository.deleteByUserName(username);
 
     }
 
