45f27e6bd2db57a4ef81e570d44e49a62df7ec73
ELİF DENK <elifdenkk@gmail.com>
2023-10-02 18:36:37 +0300

    ProjectService created

---
+++ src/main/java/com/cydeo/service/ProjectService.java
@@ -0,0 +1,17 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.ProjectDTO;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProjectService{
+  ProjectDTO getByProjectCode(String code);
+  List<ProjectDTO> listAllProjects();
+  void save(ProjectDTO dto);
+  void update(ProjectDTO dto);
+  void delete(String code);
+
+
+
+}
